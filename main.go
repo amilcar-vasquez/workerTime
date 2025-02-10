f2a785d29dd9a3b86c0ebd13e778e7cd7a6fa9e8
Amilcar Vasquez <2022156707@ub.edu.bz>
2025-02-10 13:19:40 -0600

    fixed so that it prints worker id

--- main.go
+++ main.go
@@ -5,17 +5,17 @@ import (
 	"sync"
 )
 
-func worker(wg *sync.WaitGroup) {
+func worker(wg *sync.WaitGroup, id int) {
 	defer wg.Done()
-	fmt.Println("Worker started")
-	fmt.Println("Worker done")
+	fmt.Printf("Worker %d started\n", id)
+	fmt.Printf("Worker %d done\n", id)
 }
 
 func main() {
 	var wg sync.WaitGroup
 	for i := 1; i <= 3; i++ {
 		wg.Add(1)
-		go worker(&wg)
+		go worker(&wg, i)
 	}
 	wg.Wait()
 	fmt.Println("All workers done")
